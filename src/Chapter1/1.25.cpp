e4ac1b02b7ff325baf213df556f913ece9f8c73e
believer <18763821496@163.com>
2022-07-13 23:47:06 +0800

    2022.7.13

--- src/Chapter1/1.25.cpp
+++ src/Chapter1/1.25.cpp
@@ -4,7 +4,27 @@ using namespace std;
 
 int main()
 {
-	cout<<"Hello world!"<<endl;
+	Sales_item total;
+	if (cin >> total)
+	{
+		Sales_item trans;
+		while (cin >> trans)
+		{
+			if (total.isbn() == trans.isbn())
+				total += trans;
+			else
+			{
+				cout << total << endl;
+				total = trans;
+			}
+		}
+		cout << total << endl;
+	}
+	else
+	{
+		cerr << "No data?!" << endl;
+		return -1;
+	}
 
 	system("pause");
 	return 0;
