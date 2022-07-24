f542770e5807ff7599b0ea990a093223b5971f19
believer <18763821496@163.com>
2022-07-25 00:46:02 +0800

    引用和const *

--- src/Chapter2/2.18.cpp
+++ src/Chapter2/2.18.cpp
@@ -3,26 +3,14 @@ using namespace std;
 
 int main()
 {
-	//int i = 0,j=1;
-	//int* p = &i;
-	//cout << p << endl;
-	//p = &j;
-	//cout << p << endl;
-	//cout << *p << endl;
-	//*p = 10;
-	//cout << *p << endl;
-
-	int i = 42;
-	int& r1 = i;
-	const int& r2 = i;
-	cout << r1 << " " << r2 << endl;
-	r1 = 32;
-	cout << i<<" "<< r1 << " " << r2 << endl;
-	i = 22;
-	cout << i << " " << r1 << " " << r2 << endl;
-
-
-
+	int i = 0,j=1;
+	int* p = &i;
+	cout << p << endl;
+	p = &j;
+	cout << p << endl;
+	cout << *p << endl;
+	*p = 10;
+	cout << *p << endl;
 
 	system("pause");
 	return 0;
